53695e70fc262949ce3d532cce2f9449edf20dd1
Luke Braithwaite <lukebraithwaite7@gmail.com>
2025-08-17 17:44:03 +0100

    Refactor test into a single case

--- tests/utils/test_orthogonal.py
+++ tests/utils/test_orthogonal.py
@@ -30,34 +30,25 @@ def test_invalid_transformation_type():
         Orthogonal(d=5, orthogonal_map="random_orthogonal_map")
 
 
-def test_orthogonal_matrix_exp():
-    orth_model = Orthogonal(d=5, orthogonal_map="matrix_exp")
+@pytest.mark.parametrize(
+    "orthogonal_map,stalk_dimension,in_channels",
+    [
+        ("matrix_exp", 5, 15),
+        ("cayley", 5, 15),
+        ("householder", 5, 10),
+        ("euler", 2, 1),
+        ("euler", 3, 3),
+    ],
+)
+def test_result_is_orthogonal(
+    orthogonal_map: str, stalk_dimension: int, in_channels: int
+):
+    orth_model = Orthogonal(d=stalk_dimension, orthogonal_map=orthogonal_map)
 
-    x = torch.randn((4, orth_model.d * (orth_model.d + 1) // 2))
-    out = orth_model(x)
-    assert all_orthogonal(out)
+    x = torch.randn((4, in_channels))
+    if orthogonal_map == "euler" and stalk_dimension == 3:
+        x = x.clip(-1, 1)
 
-
-def test_orthogonal_cayley():
-    orth_model = Orthogonal(d=5, orthogonal_map="cayley")
-
-    x = torch.randn((4, orth_model.d * (orth_model.d + 1) // 2))
-    out = orth_model(x)
-    assert all_orthogonal(out)
-
-
-def test_orthogonal_householder():
-    orth_model = Orthogonal(d=5, orthogonal_map="householder")
-
-    x = torch.randn((4, 10))
-    out = orth_model(x)
-    assert all_orthogonal(out)
-
-
-def test_orthogonal_euler_2d():
-    orth_model = Orthogonal(d=2, orthogonal_map="euler")
-
-    x = torch.randn((4, 1))
     out = orth_model(x)
     assert all_orthogonal(out)
 
@@ -71,14 +62,6 @@ def test_orthogonal_euler_2d_wrong_param_size():
     ):
         x = torch.randn((4, 10)).clip(-1, 1)
         orth_model(x)
-
-
-def test_orthogonal_euler_3d():
-    orth_model = Orthogonal(d=3, orthogonal_map="euler")
-
-    x = torch.randn((4, 3)).clip(-1, 1)
-    out = orth_model(x)
-    assert all_orthogonal(out)
 
 
 def test_orthogonal_euler_3d_wrong_param_size():
